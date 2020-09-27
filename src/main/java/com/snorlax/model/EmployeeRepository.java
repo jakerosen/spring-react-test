dc78c30d0af595ed37a231f83e1e69fb6262b634
jakerosen <jakesrosen@gmail.com>
2020-09-27 15:01:21 -0700

    first commit

---
+++ src/main/java/com/snorlax/model/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.snorlax.model;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository
+  extends PagingAndSortingRepository<Employee, Long> {
+
+}
